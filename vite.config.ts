326d45aa2cc6502d8cffef4da1a87dd45461a204
时瑾 <74231782+sj817@users.noreply.github.com>
2025-04-14 21:43:40 +0800

    feat: ci vite

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import fs from 'node:fs'
+import { resolve } from 'path'
+import pkg from './package.json'
+import { defineConfig } from 'vite'
+import { builtinModules } from 'node:module'
+
+/**
+ * 初始化一下package.json
+ */
+pkg.name = '@karinjs/moment'
+pkg.files = ['dist', 'moment.d.ts']
+fs.writeFileSync(
+  resolve(__dirname, 'package.json'),
+  JSON.stringify(pkg, null, 2),
+  'utf-8'
+)
+
+export default defineConfig({
+  build: {
+    target: 'es2022',
+    lib: {
+      formats: ['es'],
+      fileName: (format, entryName) => {
+        // 如果是moment主文件，直接放在dist目录下
+        if (entryName === 'moment') {
+          return `${entryName}.js`
+        }
+        // 否则为语言包，放在locale目录下
+        return `locale/${entryName}.js`
+      },
+      entry: [
+        resolve(__dirname, 'src/moment.js'),
+        // 读取src/locale文件夹下的所有js 这里的文件是moment的语言包
+        ...fs
+          .readdirSync(resolve(__dirname, 'src/locale'))
+          .filter((file) => file.endsWith('.js'))
+          .map((file) => resolve(__dirname, 'src/locale', file)),
+      ],
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        ...builtinModules,
+        ...builtinModules.map((mod) => `node:${mod}`),
+      ],
+    },
+    minify: 'terser'
+  }
+})
